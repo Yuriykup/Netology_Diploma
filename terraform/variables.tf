e1960d8537f1054ecad7821a0081c6ff623a1ef6
Yuriykup <yor1k@yandex.ru>
2026-06-07 10:10:14 +0000

    Блок провайдера вынесен в отдельный файл provider.tf. Отредактирован файл README.md

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "flow" {
+  type    = string
+  default = "vpc-network"
+}
+
+# Идентификатор облака netology-diploma
+variable "cloud_id" {
+  type    = string
+  default = "b1gctgc3mduvdrtvv7o6"
+}
+
+#Идентификатор папки default
+variable "folder_id" {
+  type    = string
+  default = "1gpo0761hhdt23vcd7p""
+}
