366311be429a379792875abe97c6148014daf84c
Yuriykup <yor1k@yandex.ru>
2026-06-29 15:22:34 +0700

    2026.06.29 - Рабочий проект и описание

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,92 +1,112 @@
-#Application Load Balancer - сетевого балансировщика
+# ============================================
+# Application Load Balancer
+# ============================================
 
-#Целевая группа для ALB. Определены IP адреса WEB серверов
-resource "yandex_alb_target_group" "web_target_group" {  # Создание ресурса "целевая группа" с локальным именем web_target_group
-  name = "target-group"  # Отображаемое имя целевой группы в облаке
+# Целевая группа для ALB
+resource "yandex_alb_target_group" "web_target_group" {
+  name = "target-group"
 
-  target {  
-    subnet_id          = yandex_vpc_subnet.develop_a.id  # ID подсети, в которой находится первая ВМ (зона А)
-    ip_address         = yandex_compute_instance.weba.network_interface.0.ip_address  # Внутренний IP адрес ВМ weba
+  target {
+    subnet_id  = yandex_vpc_subnet.develop_a.id
+    ip_address = yandex_compute_instance.weba.network_interface.0.ip_address
   }
 
-  target {  
-    subnet_id          = yandex_vpc_subnet.develop_b.id  # ID подсети, в которой находится вторая ВМ (зона Б)
-    ip_address         = yandex_compute_instance.webb.network_interface.0.ip_address  # Внутренний IP адрес ВМ webb
+  target {
+    subnet_id  = yandex_vpc_subnet.develop_b.id
+    ip_address = yandex_compute_instance.webb.network_interface.0.ip_address
   }
 }
 
-# Создание ресурса "бэкенд группа"
-resource "yandex_alb_backend_group" "web_backend_group" {  
-  name  =  "backend-group"  # Отображаемое имя бэкенд группы в облаке
+# Бэкенд группа
+resource "yandex_alb_backend_group" "web_backend_group" {
+  name = "backend-group"
 
   http_backend {
-    name                = "http-backend"  # Имя этого бэкенда
-    port                = 80  # Порт на целевых ВМ, на который будет направляться трафик (HTTP)
-    target_group_ids    = ["${yandex_alb_target_group.web_target_group.id}"]  # ID целевой группы, в которую входят веб-сервера
-    
+    name             = "http-backend"
+    port             = 80
+    target_group_ids = [yandex_alb_target_group.web_target_group.id]
+
     healthcheck {
-      timeout           = "10s"  # Таймаут ожидания ответа от ВМ при проверке здоровья (10 секунд)
-      interval          = "5s"  # Интервал между проверками здоровья (каждые 5 секунд)
-      healthcheck_port  = 80  # Порт, на котором проверяется здоровье ВМ (HTTP порт 80)
+      timeout  = "10s"
+      interval = "5s"
       http_healthcheck {
-        path = "/"  # Путь URL для проверки здоровья (корневой путь "/")
-      } 
+        path              = "/"
+        expected_statuses = [200]
+      }
     }
   }
 }
 
-# HTTP роутер - правила маршрутизации запросов
-resource "yandex_alb_http_router" "web_router" {  
-  name = "web-router"  # Отображаемое имя HTTP роутера в облаке
+# HTTP роутер
+resource "yandex_alb_http_router" "web_router" {
+  name = "web-router"
 }
 
-# Виртуальный хост - настройки для домена
+# Виртуальный хост
 resource "yandex_alb_virtual_host" "web_vhost" {
-  name           = "web-vhost"  # Отображаемое имя виртуального хоста в облаке
-  http_router_id = yandex_alb_http_router.web_router.id  # ID HTTP роутера, к которому привязывается этот виртуальный хост
-  
+  name           = "web-vhost"
+  http_router_id = yandex_alb_http_router.web_router.id
+
   route {
-    name = "default-route"  # Имя маршрута
+    name = "default-route"
     http_route {
       http_route_action {
-        backend_group_id = yandex_alb_backend_group.web_backend_group.id  # ID бэкенд группы, на которую направлять трафик
-        timeout          = "5s"  # Таймаут ожидания ответа от бэкенда (5 секунд)
+        backend_group_id = yandex_alb_backend_group.web_backend_group.id
+        timeout          = "5s"
       }
     }
   }
 }
 
-# Создание ресурса "Application Load Balancer"
-resource "yandex_alb_load_balancer" "alb-load-balancer" {
-  name = "alb-load-balancer"  # Отображаемое имя балансировщика в облаке
-  network_id = yandex_vpc_network.develop.id  # ID сети, в которой будет размещён балансировщик
-  security_group_ids = [yandex_vpc_security_group.alb-load-balancer-sg.id]  # Список ID групп безопасности для балансировщика
+# Application Load Balancer
+resource "yandex_alb_load_balancer" "alb_load_balancer" {
+  name        = "alb-load-balancer"
+  network_id  = yandex_vpc_network.develop.id
+  security_group_ids = [yandex_vpc_security_group.alb-load-balancer-sg.id]
 
-  allocation_policy {  #в каких зонах работать
+  depends_on = [
+    yandex_alb_target_group.web_target_group,
+    yandex_alb_backend_group.web_backend_group,
+    yandex_alb_http_router.web_router,
+    yandex_alb_virtual_host.web_vhost,
+    yandex_compute_instance.weba,
+    yandex_compute_instance.webb
+  ]
+
+  allocation_policy {
     location {
-      zone_id = "ru-central1-a"  # Зона доступности А
-      subnet_id = yandex_vpc_subnet.develop_a.id  # ID подсети в зоне А для размещения балансировщика
+      zone_id   = "ru-central1-a"
+      subnet_id = yandex_vpc_subnet.develop_a.id
     }
 
     location {
-      zone_id = "ru-central1-b"  # Зона доступности Б
-      subnet_id = yandex_vpc_subnet.develop_b.id  # ID подсети в зоне Б для размещения балансировщика
-    } 
-  } 
+      zone_id   = "ru-central1-b"
+      subnet_id = yandex_vpc_subnet.develop_b.id
+    }
+  }
 
-  listener {  #точки входа трафика
-    name = "listener"  # Имя слушателя
+  listener {
+    name = "listener"
     endpoint {
       address {
-        external_ipv4_address {  # Использовать внешний IPv4 адрес (публичный IP)
-        } 
+        external_ipv4_address {}
       }
-      ports = [80]  # Список портов, на которых принимается трафик (порт 80 - HTTP)
+      ports = [80]
     }
-    http { 
+    http {
       handler {
-        http_router_id = yandex_alb_http_router.web_router.id  # ID HTTP роутера, который будет обрабатывать входящие запросы
-      } 
-    } 
-  } 
-} 
+        http_router_id = yandex_alb_http_router.web_router.id
+      }
+    }
+  }
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,39 +1,64 @@
 resource "local_file" "inventory" {
-  content  = <<-XYZ
+  content = <<-XYZ
+  # ============================================
+  # ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
+  # ============================================
   [all:vars]
   ansible_user=kupriyanov
   ansible_ssh_private_key_file=~/.ssh/id_ed25519
-  ansible_ssh_common_args='-o ProxyCommand="ssh -p 22 -W %h:%p -q kupriyanov@${yandex_compute_instance.bastion.network_interface.0.nat_ip_address}"'
+  ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 
-  application-load-balancer=${yandex_alb_load_balancer.alb-load-balancer.listener[0].endpoint[0].address[0].external_ipv4_address[0].address}:80
+  # Адрес балансировщика
+  alb_address=${yandex_alb_load_balancer.alb_load_balancer.listener[0].endpoint[0].address[0].external_ipv4_address[0].address}:80
 
-  bastion=${yandex_compute_instance.bastion.network_interface.0.nat_ip_address}
-  
-  weba=${yandex_compute_instance.weba.network_interface.0.ip_address}
-  webb=${yandex_compute_instance.webb.network_interface.0.ip_address}
+  # IP-адреса хостов
+  bastion_ip=${yandex_compute_instance.bastion.network_interface.0.nat_ip_address}
+  weba_ip=${yandex_compute_instance.weba.network_interface.0.ip_address}
+  webb_ip=${yandex_compute_instance.webb.network_interface.0.ip_address}
+  elasticsearch_ip=${yandex_compute_instance.elasticsearch.network_interface.0.ip_address}
+  zabbix_ip=${yandex_compute_instance.zabbix-server.network_interface.0.nat_ip_address}
+  kibana_ip=${yandex_compute_instance.kibana-server.network_interface.0.nat_ip_address}
 
-  elasticsearch=${yandex_compute_instance.elasticsearch.network_interface.0.ip_address}
-
-  zabbix-server=${yandex_compute_instance.zabbix-server.network_interface.0.nat_ip_address}
-
-  kibana-server=${yandex_compute_instance.kibana-server.network_interface.0.nat_ip_address}
-
-
+  # ============================================
+  # БАСТИОН
+  # ============================================
   [bastion]
-  bastion-server ansible_host=bastion.ru-central1.internal
+  bastion-server ansible_host=${yandex_compute_instance.bastion.network_interface.0.nat_ip_address}
 
-  [elasticsearch-server]
-  elasticsearch ansible_host=elasticsearch.ru-central1.internal
+  # ============================================
+  # ELASTICSEARCH
+  # ============================================
+  [elasticsearch_servers]
+  elasticsearch ansible_host=${yandex_compute_instance.elasticsearch.network_interface.0.ip_address}
 
-  [kibana]
-  kibana-server ansible_host=kibana-server.ru-central1.internal
+  # ============================================
+  # KIBANA
+  # ============================================
+  [kibana_servers]
+  kibana-server ansible_host=${yandex_compute_instance.kibana-server.network_interface.0.nat_ip_address}
 
-  [hosts]
-  weba ansible_host=weba.ru-central1.internal
-  webb ansible_host=webb.ru-central1.internal
-  
-  [monitoring]
-  zabbix-server ansible_host=zabbix-server.ru-central1.internal
+  # ============================================
+  # ВЕБ-СЕРВЕРЫ
+  # ============================================
+  [web_servers]
+  weba ansible_host=${yandex_compute_instance.weba.network_interface.0.ip_address}
+  webb ansible_host=${yandex_compute_instance.webb.network_interface.0.ip_address}
+
+  # ============================================
+  # ZABBIX
+  # ============================================
+  [zabbix_servers]
+  zabbix-server ansible_host=${yandex_compute_instance.zabbix-server.network_interface.0.nat_ip_address}
+
+  # ============================================
+  # ПРОКСИ ЧЕРЕЗ БАСТИОН
+  # ============================================
+  [web_servers:vars]
+  ansible_ssh_common_args='-o ProxyCommand="ssh -i ~/.ssh/id_ed25519 -W %h:%p -q kupriyanov@${yandex_compute_instance.bastion.network_interface.0.nat_ip_address}" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+
+  [elasticsearch_servers:vars]
+  ansible_ssh_common_args='-o ProxyCommand="ssh -i ~/.ssh/id_ed25519 -W %h:%p -q kupriyanov@${yandex_compute_instance.bastion.network_interface.0.nat_ip_address}" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
   XYZ
+
   filename = "../ansible/hosts.ini"
 }
